59f114b573fabbfd4ce02ac8af6141eecadcdc9c
naylorc <40211677+naylorc@users.noreply.github.com>
2018-06-12 12:55:35 -0400

    Add files via upload

---
+++ P2T1_NaylorConnor.py
@@ -0,0 +1,8 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Connor Naylor
+#6/12/18
+
+total_sales = float(input('Enter the projected sales: '))
+profit = (total_sales*0.23)
+print ('The profit is', profit)
